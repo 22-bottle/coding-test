355b9a437b2bb3db39f601631ae7a6e539f1f52b
22-bottle <101461544+22-bottle@users.noreply.github.com>
2022-12-30 00:31:52 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,6 +1,6 @@
 def solution(k, m, score):
     answer = 0
-    left = 0
+    '''left = 0
     for i in range(k, 0, -1):
         i_apples = score.count(i)
         if i_apples >= m - left:
@@ -11,5 +11,10 @@ def solution(k, m, score):
             left += i_apples
             box = 0
         box += i_apples // m
-        answer += box * m * i
+        answer += box * m * i'''
+    
+    score.sort(reverse=True)
+    for i in range(len(score) // m):
+        box = score[i*m:(i+1)*m]
+        answer += min(box) * m
     return answer
