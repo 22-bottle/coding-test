6416f645f7e2a0cba57f95e566f8c32acc33a5b3
22-bottle <101461544+22-bottle@users.noreply.github.com>
2022-12-30 00:37:01 +0900

    Update 과일 장수.py

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,18 +1,5 @@
 def solution(k, m, score):
     answer = 0
-    '''left = 0
-    for i in range(k, 0, -1):
-        i_apples = score.count(i)
-        if i_apples >= m - left:
-            i_apples -= m - left
-            box = 1
-            left = i_apples % m
-        else:
-            left += i_apples
-            box = 0
-        box += i_apples // m
-        answer += box * m * i'''
-    
     score.sort(reverse=True)
     for i in range(len(score) // m):
         box = score[i*m:(i+1)*m]
