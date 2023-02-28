3c73662c3e699bc40cdec316bc54e00627511479
22-bottle <101461544+22-bottle@users.noreply.github.com>
2023-02-28 21:43:51 +0900

    Update 구명보트.py

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,11 +1,12 @@
 # n이 50000 이하 이므로 O(n^2)까지 가능
+# 배열 수정하지 않아도 될 때는 굳이 수정할 필요 없음
 
 def solution(people, limit):
     answer = 0
     people.sort() # 정렬 -> O(nlogn)
     l = 0
     r = len(people) - 1
-    while l < r:
+    while l < r: # 최악의 경우 n번 검사 -> O(n)
         if people[l] + people[r] <= limit:
             l += 1
             r -= 1
