d675f9af18a2d7b3d14f5222160980e72a012b73
22-bottle <101461544+22-bottle@users.noreply.github.com>
2023-02-28 21:40:59 +0900

    [level 2] Title: 구명보트, Time: 13.84 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+# n이 50000 이하 이므로 O(n^2)까지 가능
+
+def solution(people, limit):
+    answer = 0
+    people.sort() # 정렬 -> O(nlogn)
+    l = 0
+    r = len(people) - 1
+    while l < r:
+        if people[l] + people[r] <= limit:
+            l += 1
+            r -= 1
+        else:
+            r -= 1
+        answer += 1
+    if l == r:
+        answer += 1
+    return answer
