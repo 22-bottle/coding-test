7a2b70ce80e2f944a8a87d15fb15f8d372b33089
22-bottle <101461544+22-bottle@users.noreply.github.com>
2023-02-28 22:23:21 +0900

    Update 단속카메라.py

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -1,9 +1,11 @@
+# n이 10000 이하 이므로 O(n^2)까지 가능
+
 def solution(routes):
     answer = 0
-    routes.sort(key=lambda x:x[1])
-    camera = -30001
+    routes.sort(key=lambda x:x[1]) # 정렬 -> O(nlogn)
+    camera = -30001 # 마지막 카메라의 위치
     for route in routes:
-        if route[0] > camera:
-            camera = route[1]
+        if route[0] > camera: # 카메라의 위치를 벗어나면
+            camera = route[1] # 카메라 추가 설치
             answer += 1
     return answer
