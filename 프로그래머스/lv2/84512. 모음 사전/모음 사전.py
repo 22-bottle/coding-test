d671535feff6483a0dca0b10fb3946c5ef6b8747
22-bottle <101461544+22-bottle@users.noreply.github.com>
2023-03-03 14:59:47 +0900

    Update 모음 사전.py

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -7,10 +7,10 @@ def solution(word):
         if len(my_word) < 5: # 길이가 5보다 작으면 A 추가
             my_word.append('A')
         else:
-            for i in range(4, -1, -1):
-                if my_word[i] == 'U':
+            for i in range(4, -1, -1): # 각 자리를 돌며
+                if my_word[i] == 'U': # U면 그 자리 없애기
                     my_word.pop(i)
-                else:
+                else: # 다른 문자는 다음 문자로 변경
                     my_word[i] = next_word[my_word[i]]
                     break
         answer += 1
