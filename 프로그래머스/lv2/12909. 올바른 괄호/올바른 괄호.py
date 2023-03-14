212baf334b9f8374c06646146d68c94d2b87971e
22-bottle <101461544+22-bottle@users.noreply.github.com>
2023-03-14 12:49:05 +0900

    Update 올바른 괄호.py

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,7 +1,7 @@
 def solution(s):
     stack = 0
-    for i in range(len(s)):
-        if s[i] == '(':
+    for c in s:
+        if c == '(':
             stack += 1
         else:
             if stack <= 0:
