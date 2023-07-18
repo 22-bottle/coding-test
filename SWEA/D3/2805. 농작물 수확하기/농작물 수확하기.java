7eb3378ff7012f6c0b0f79f04ee483ec3e58f530
22-bottle <101461544+22-bottle@users.noreply.github.com>
2023-07-18 22:48:04 +0900

    [D3] Title: 농작물 수확하기, Time: 173 ms, Memory: 29,488 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+ 
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+ 
+        for(int test_case = 1; test_case <= T; test_case++) {
+            int N = sc.nextInt();
+            int[][] map = new int[N][N];
+            for (int i = 0; i < N; i++) {
+            	String str = sc.next();
+            	for (int j = 0; j < N; j++) {
+            		map[i][j] = str.charAt(j) - '0';
+            	}
+            }
+        	
+            int half = N / 2;
+            int start = half;
+            int count = 1;
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+            	for (int j = 0; j < count; j++) {
+            		sum += map[i][start + j];
+            	}
+            	if (i < half) {
+            		start -= 1;
+            		count += 2;
+            	}
+            	else {
+            		start += 1;
+            		count -= 2;
+            	}
+            }
+        	System.out.printf("#%d %d\n", test_case, sum);
+        }
+    }
+}
