faac520e91065eef909aabaeca6aa76ad5813696
22-bottle <101461544+22-bottle@users.noreply.github.com>
2023-08-14 17:16:49 +0900

    [D3] Title: 상호의 배틀필드, Time: 258 ms, Memory: 48,428 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -1,96 +1,115 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.*;
 public class Solution {
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			int r = 0, c = 0;
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int H = Integer.parseInt(st.nextToken());
-			int W = Integer.parseInt(st.nextToken());
-			char[][] map = new char[H][W];
-			for (int i = 0; i < H; i++) {
-				char[] temp = br.readLine().toCharArray();
-				for (int j = 0; j < W; j++) {
-					map[i][j] = temp[j];
-					if (temp[j] == '^' || temp[j] == 'v' || temp[j] == '<' || temp[j] == '>') {
-						r = i;
-						c = j;
-					}
-				}
-			}
-			int N = Integer.parseInt(br.readLine());
-			String commands = br.readLine();
+	static int[] dr = {-1, 1, 0, 0};  //상 하 좌 우
+	static int[] dc = {0, 0, -1, 1};
+	static int dir;
+	static int row, col;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for (int tc = 1; tc <= t; tc++) {
+			int h = sc.nextInt();
+			int w = sc.nextInt();
+			String[][] map = new String[h][w];
 			
-			int[] dr = { -1, 1, 0, 0 };
-			int[] dc = { 0, 0, -1, 1 };
-			for (int i = 0; i < N; i++) {
-				switch (commands.charAt(i)) {
-				case 'U':
-					if (r - 1 >= 0 && map[r - 1][c] == '.') {
-						map[r--][c] = '.';
-					}
-					map[r][c] = '^';
-					break;
-				case 'D':
-					if (r + 1 < H && map[r + 1][c] == '.') {
-						map[r++][c] = '.';
-					}
-					map[r][c] = 'v';
-					break;
-				case 'L':
-					if (c - 1 >= 0 && map[r][c - 1] == '.') {
-						map[r][c--] = '.';
-					}
-					map[r][c] = '<';
-					break;
-				case 'R':
-					if (c + 1 < W && map[r][c + 1] == '.') {
-						map[r][c++] = '.';
-					}
-					map[r][c] = '>';
-					break;
-				case 'S':
-					int d = -1;
-					int missile_r = r, missile_c = c;
-					switch (map[r][c]) {
-					case '^':
-						d = 0;
-						break;
-					case 'v':
-						d = 1;
-						break;
-					case '<':
-						d = 2;
-						break;
-					case '>':
-						d = 3;
-						break;
-					}
-					while (missile_r >= 0 && missile_r < H && missile_c >= 0 && missile_c < W) {
-						if (map[missile_r][missile_c] == '*') {
-							map[missile_r][missile_c] = '.';
-							break;
-						} else if (map[missile_r][missile_c] == '#') {
-							break;
-						}
-						missile_r += dr[d];
-						missile_c += dc[d];
+			for (int i = 0; i < h; i++) {
+				map[i] = sc.next().split("");
+			}
+			
+			int n = sc.nextInt();
+			String[] cmd = sc.next().split("");
+			
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++) {
+					if(map[i][j].equals("^") || map[i][j].equals("<") || map[i][j].equals(">") || map[i][j].equals("v")) {
+						if(map[i][j].equals("^")) dir = 0;
+						else if(map[i][j].equals("<")) dir = 2;
+						else if(map[i][j].equals(">"))  dir = 3;
+						else if(map[i][j].equals("v")) dir = 1;
+						row = i;
+						col = j;
 					}
 				}
 			}
-			System.out.printf("#%d ", tc);
-			for (int i = 0; i < H; i++) {
-				for (int j = 0; j < W; j++) {
+
+			int nr, nc;
+			for (int c = 0; c < cmd.length; c++) {
+				switch (cmd[c]) {
+					case "U":
+						map[row][col] = "^";
+						dir = 0;
+						nr = row + dr[dir];
+						nc = col + dc[dir];
+						if(nr >= 0 && nr < h && nc >= 0 && nc < w && map[nr][nc].equals(".")) {
+							map[row][col] = ".";
+							map[nr][nc] = "^";
+							row = nr;
+							col = nc;
+						}
+						break;
+					
+					case "D":
+						map[row][col] = "v";
+						dir = 1;
+						nr = row + dr[dir];
+						nc = col + dc[dir];
+						if(nr >= 0 && nr < h && nc >= 0 && nc < w && map[nr][nc].equals(".")) {
+							map[row][col] = ".";
+							map[nr][nc] = "v";
+							row = nr;
+							col = nc;
+						}
+						break;
+						
+					case "L":
+						map[row][col] = "<";
+						dir = 2;
+						nr = row + dr[dir];
+						nc = col + dc[dir];
+						if(nr >= 0 && nr < h && nc >= 0 && nc < w && map[nr][nc].equals(".")) {
+							map[row][col] = ".";
+							map[nr][nc] = "<";
+							row = nr;
+							col = nc;
+						}
+						break;
+						
+					case "R":
+						map[row][col] = ">";
+						dir = 3;
+						nr = row + dr[dir];
+						nc = col + dc[dir];
+						if(nr >= 0 && nr < h && nc >= 0 && nc < w && map[nr][nc].equals(".")) {
+							map[row][col] = ".";
+							map[nr][nc] = ">";
+							row = nr;
+							col = nc;
+						}
+						break;
+					
+					case "S":
+						nr = row + dr[dir];
+						nc = col + dc[dir];
+						while(nr >= 0 && nr < h && nc >= 0 && nc < w) {
+							if(map[nr][nc].equals("*")) {
+								map[nr][nc] = ".";
+								break;
+							}
+							else if(map[nr][nc].equals("#")) break;
+							nr += dr[dir];
+							nc += dc[dir];
+						}
+						break;
+				}
+			}
+			System.out.print("#" + tc + " ");
+			for (int i = 0; i < h; i++) {
+				for (int j = 0; j < w; j++) {
 					System.out.print(map[i][j]);
 				}
 				System.out.println();
 			}
 		}
 	}
-
 }
