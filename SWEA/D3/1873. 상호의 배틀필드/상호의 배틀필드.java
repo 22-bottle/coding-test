3c48093831dd4ebb1fb0926ef170a2d040c10741
22-bottle <101461544+22-bottle@users.noreply.github.com>
2023-08-14 17:50:49 +0900

    [D3] Title: 상호의 배틀필드, Time: 263 ms, Memory: 51,472 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -2,8 +2,6 @@ import java.util.*;
 public class Solution {
 	static int[] dr = {-1, 1, 0, 0};  //상 하 좌 우
 	static int[] dc = {0, 0, -1, 1};
-	static int dir;
-	static int row, col;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
@@ -11,6 +9,7 @@ public class Solution {
 		for (int tc = 1; tc <= t; tc++) {
 			int h = sc.nextInt();
 			int w = sc.nextInt();
+			int dir = 0;  // 탱크의 방향
 			String[][] map = new String[h][w];
 			
 			for (int i = 0; i < h; i++) {
@@ -20,20 +19,22 @@ public class Solution {
 			int n = sc.nextInt();
 			String[] cmd = sc.next().split("");
 			
+			int row = 0;
+			int col = 0;
 			for (int i = 0; i < h; i++) {
 				for (int j = 0; j < w; j++) {
 					if(map[i][j].equals("^") || map[i][j].equals("<") || map[i][j].equals(">") || map[i][j].equals("v")) {
 						if(map[i][j].equals("^")) dir = 0;
+						else if(map[i][j].equals("v")) dir = 1;
 						else if(map[i][j].equals("<")) dir = 2;
 						else if(map[i][j].equals(">"))  dir = 3;
-						else if(map[i][j].equals("v")) dir = 1;
 						row = i;
 						col = j;
 					}
 				}
 			}
-
-			int nr, nc;
+			
+			int nr, nc;  //  다음 칸의 좌표
 			for (int c = 0; c < cmd.length; c++) {
 				switch (cmd[c]) {
 					case "U":
