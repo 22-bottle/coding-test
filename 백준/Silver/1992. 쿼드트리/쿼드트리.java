d82070ee17fe46d91e7e6a27526f587e999674fb
22-bottle <101461544+22-bottle@users.noreply.github.com>
2023-08-16 17:20:50 +0900

    [Silver I] Title: 쿼드트리, Time: 148 ms, Memory: 15548 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,49 +1,106 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-	static int n;
-	static char[][] map;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		map = new char[n][n];
-		
-		for (int i = 0; i < n; i++) {
-			map[i] = sc.next().toCharArray();
-		}
-		quadTree(0, 0, n);
-		
-	}
-	
-	private static void quadTree(int r, int c, int size) {
-		
-		if(checkNum(r, c, size)) {
-			if(map[r][c] == '0') System.out.print(0);
-			else System.out.print(1);
-			return;
-		}
-		
-		int newSize = size / 2;
-		System.out.print("(");
-		quadTree(r, c, newSize);  // 2사분면
-		quadTree(r, c+newSize, newSize);  // 1사분면
-		quadTree(r+newSize, c, newSize);  // 3사분면
-		quadTree(r+newSize, c+newSize, newSize);  // 4사분면
-		System.out.print(")");
-	}
-	
-	private static boolean checkNum(int r, int c, int size) {
-		char firstNum = map[r][c];
-		
-		for (int row = r; row < r+size; row++) {
-			for (int col = c; col < c+size; col++) {
-				if(map[row][col] != firstNum) {
-					return false;
-				}
+	static StringBuilder sb;
+	static int[][] video;
+	static boolean is_zero, is_one;
+	static int[] quardrant;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		video = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			String[] temp = br.readLine().split("");
+			for (int j = 0; j < N; j++) {
+				video[i][j] = Integer.parseInt(temp[j]);
 			}
 		}
-		return true;
+		
+		sb = new StringBuilder();
+		quardrant = new int[4];
+		can_zip(0, 0, N);
+		System.out.println(sb);
+	}
+	
+	private static void can_zip(int r, int c, int side) {
+		if (side == 1) {
+			sb.append(video[r][c]);
+			return;
+		}
+		for (int i = 0; i < 4; i++) {
+			quardrant[i] = -5;
+		}
+		int half = side >> 1;
+		
+		is_zero = false;
+		is_one = false;
+		for (int i = r; i < r + half; i++) {
+			for (int j = c; j < c + half; j++) {
+				if (video[i][j] == 0) is_zero = true;
+				else is_one = true;
+				if (is_zero && is_one) break;
+			}
+			if (is_zero && is_one) break;
+		}
+		if (is_zero && !is_one) quardrant[0] = 0;
+		else if (!is_zero && is_one) quardrant[0] = 1;
+		
+		is_zero = false;
+		is_one = false;
+		for (int i = r; i < r + half; i++) {
+			for (int j = c + half; j < c + half * 2; j++) {
+				if (video[i][j] == 0) is_zero = true;
+				else is_one = true;
+				if (is_zero && is_one) break;
+			}
+			if (is_zero && is_one) break;
+		}
+		if (is_zero && !is_one) quardrant[1] = 0;
+		else if (!is_zero && is_one) quardrant[1] = 1;
+		
+		is_zero = false;
+		is_one = false;
+		for (int i = r + half; i < r + half * 2; i++) {
+			for (int j = c; j < c + half; j++) {
+				if (video[i][j] == 0) is_zero = true;
+				else is_one = true;
+				if (is_zero && is_one) break;
+			}
+			if (is_zero && is_one) break;
+		}
+		if (is_zero && !is_one) quardrant[2] = 0;
+		else if (!is_zero && is_one) quardrant[2] = 1;
+		
+		is_zero = false;
+		is_one = false;
+		for (int i = r + half; i < r + half * 2; i++) {
+			for (int j = c + half; j < c + half * 2; j++) {
+				if (video[i][j] == 0) is_zero = true;
+				else is_one = true;
+				if (is_zero && is_one) break;
+			}
+			if (is_zero && is_one) break;
+		}
+		if (is_zero && !is_one) quardrant[3] = 0;
+		else if (!is_zero && is_one) quardrant[3] = 1;
+		
+		int sum = 0;
+		for (int i = 0; i < 4; i++) {
+			sum += quardrant[i];
+		}
+		if (sum == 0) sb.append(0);
+		else if (sum == 4) sb.append(1);
+		else {
+			sb.append("(");
+			can_zip(r, c, half);
+			can_zip(r, c + half, half);
+			can_zip(r + half, c, half);
+			can_zip(r + half, c + half, half);
+			sb.append(")");
+		}
 	}
 
 }
