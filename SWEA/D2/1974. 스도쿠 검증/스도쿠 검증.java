e77a12e6c0c476cd19a1ab44f4bdff07033ba349
22-bottle <101461544+22-bottle@users.noreply.github.com>
2023-08-16 15:04:57 +0900

    [D2] Title: 스도쿠 검증, Time: 106 ms, Memory: 18,388 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,48 +1,81 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class  Solution{
-	static int[][] map = new int[9][9];
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
+public class Solution {
+	
+	static int[][] sudoku;
+	static boolean answer;
+	static boolean[] isVisited;
+
+	public static void main(String[] args) throws Exception {
 		
-		for (int tc = 1; tc <= t; tc++) {
-			int answer = 1;
-			
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			sudoku = new int[9][9];
+			isVisited = new boolean[10];
 			for (int i = 0; i < 9; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 				for (int j = 0; j < 9; j++) {
-					map[i][j] = sc.nextInt();
+					sudoku[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			for (int row = 0; row < 9; row++) {
-				int rowSum = 0;
-				int colSum = 0;
-				for (int col = 0; col < 9; col++) {
-					rowSum += map[row][col];
-					colSum += map[col][row];
-				}
-				if(rowSum != 45 || colSum != 45) {
-					answer = 0;
-					break;
-				}
-			}
+			answer = true;
+			check_row();
+			if (answer) check_column();
+			if (answer) check_square();
 			
-			for (int i = 0; i < 9; i+=3) {
-				for (int j = 0; j < 9; j+=3) {
-					int arrSum = 0;
-					for (int x = i; x < i+3; x++) {
-						for (int y = j; y < j+3; y++) {
-							arrSum += map[x][y];
-						}
-					}					
-					if(arrSum != 45) {
-						answer = 0;
-						break;
-					}
-				}
-			}
-			System.out.println("#" + tc + " " + answer);
+			if (answer) System.out.printf("#%d 1\n", tc);
+			else System.out.printf("#%d 0\n", tc);
 		}
 	}
+	
+	private static void check_row() {
+		for (int i = 0; i < 9; i++) {
+			if (!answer) break;
+			for (int j = 1; j < 10; j++) {
+				isVisited[j] = false;				
+			}
+			
+			for (int j = 0; j < 9; j++) {
+				if (isVisited[sudoku[i][j]]) answer = false;
+				else isVisited[sudoku[i][j]] = true;
+			}
+		}
+	}
+	
+	private static void check_column() {
+		for (int j = 0; j < 9; j++) {
+			if (!answer) break;
+			for (int i = 1; i < 10; i++) {
+				isVisited[i] = false;
+			}
+			
+			for (int i = 0; i < 9; i++) {
+				if (isVisited[sudoku[i][j]]) answer = false;
+				else isVisited[sudoku[i][j]] = true;
+			}
+		}
+	}
+	
+	private static void check_square() {
+		for (int i = 0; i < 9; i++) {
+			if (!answer) break;
+			for (int j = 1; j < 10; j++) {
+				isVisited[j] = false;
+			}
+			
+			int sr = (i / 3) * 3;
+			int sc = (i % 3) * 3;
+			for (int j = 0; j < 9; j++) {
+				int wr = (j / 3);
+				int wc = (j % 3);
+				if (isVisited[sudoku[sr + wr][sc + wc]]) answer = false;
+				else isVisited[sudoku[sr + wr][sc + wc]] = true;
+			}
+		}
+	}
+
 }
