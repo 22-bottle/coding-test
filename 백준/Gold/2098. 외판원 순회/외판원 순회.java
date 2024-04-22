a7e7606e2fb5779326ac2b5e05caa874f4f9a22b
22-bottle <101461544+22-bottle@users.noreply.github.com>
2024-04-22 14:35:43 +0900

    [Gold I] Title: 외판원 순회, Time: 148 ms, Memory: 16504 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,51 +1,84 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static final int INF = 16_000_000;
-    static int[][] W, dp;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = null;
-        N = Integer.parseInt(br.readLine());
-        W = new int[N][N];
-        for(int i=0;i<N;i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<N;j++) W[i][j] = Integer.parseInt(st.nextToken());
-        }
+	
+	static int N;
+	static int[][] dist, mem, path;
+	static final int INF = 16_000_000;
 
-        dp = new int[N][(1<<N)-1];
-        for(int i=0;i<N;i++) Arrays.fill(dp[i], -1);
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		N = Integer.parseInt(br.readLine());
+		dist = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				dist[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		int size = 1 << (N - 1);
+		mem = new int[N][size];
+		path = new int[N][size];
+		for (int i = 1; i < N; i++) {
+			if (dist[i][0] == 0) mem[i][0] = INF;
+			else mem[i][0] = dist[i][0];
+		}
+		for (int k = 1; k < N; k++) {
+			for (int A = 1; A < size - 1; A++) {
+				if (count(A) == k) {
+					for (int i = 0; i < N - 1; i++) { //i + 1번째 노드가 포함되는지
+						if ((A & (1 << i)) == 0) {
+							minimum(i + 1, A);
+						}
+					}
+				}
+			}
+		}
+		minimum(0, size - 1);
+//		for (int i = 0; i < N; i++) {
+//			for (int j = 0; j < size; j++) {
+//				System.out.print(path[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+		System.out.println(mem[0][size - 1]);
+		
+	}
+	
+	private static void minimum(int i, int A) {
+		int minValue = INF;
+		int minJ = 0;
+		for (int j = 0; j < N - 1; j++) {
+			if ((A & (1 << j)) != 0) {
+				if (dist[i][j + 1] == 0) continue;
+				int m = dist[i][j + 1] + mem[j + 1][diff(A, j)];
+				if (minValue > m) {
+					minValue = m;
+					minJ = j + 1;
+				}
+			}
+		}
+		mem[i][A] = minValue;
+		path[i][A] = minJ;
+	}
+	
+	private static int diff(int A, int j) {
+		int t = 1 << j;
+		return (A & (~t));
+	}
+	
+	private static int count(int A) {
+		int count = 0;
+		for (int i = 0; i < N; i++) {
+			if ((A & (1 << i)) != 0) {
+				count++;
+			}
+		}
+		return count;
+	}
 
-        bw.write(dfs(0, 1)+"\n");
-        bw.flush();
-        br.close();
-    }
-
-    static int dfs(int now, int visit) {
-
-        // 모든 도시를 지난 경우
-        if(visit == (1<<N)-1) {
-            // now -> 0(출발도시)로 가는 경로가 존재해야 돌아갈 수 있음
-            if(W[now][0] == 0) return INF;
-            return W[now][0];
-        }
-
-        if(dp[now][visit] != -1) return dp[now][visit];
-        dp[now][visit] = INF;
-
-        for(int i=0;i<N;i++) {
-            // now -> 아직 방문하지 않는 i번 도시 가는 경로가 있는 경우
-            if((visit & (1<<i)) == 0 && W[now][i] != 0) {
-                // d[i][j] = 현재 있는 도시가 i이고 이미 방문한 도시들의 집합이 j일때,
-                // 방문하지 않은 나머지 도시들을 모두 방문한 뒤 출발 도시로 돌아올 때 드는 최소 비용.
-                // 즉, 방문해야하는 도시(여기에 시작지점으로 돌아오는 것 포함) 들까지 가는 최소 비용
-                dp[now][visit] = Math.min(dfs(i, visit | (1 << i)) + W[now][i], dp[now][visit]);   // 최소비용 갱신
-                // dfs(다음 도시, 다음도시 방문했다고 가정) + 여기서 다음 도시까지의 거리 와 최소거리 비교
-            }
-        }
-        return dp[now][visit];
-    }
 }
