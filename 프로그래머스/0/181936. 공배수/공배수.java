34a20e3390346049807f9145aeba0e202416cc7c
이희병 <101461544+22-bottle@users.noreply.github.com>
2024-07-09 16:35:15 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int number, int n, int m) {
+        if (number % n == 0 && number % m == 0) return 1;
+        else return 0;
+    }
+}
