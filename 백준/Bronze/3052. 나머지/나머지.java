32bca709ab66f2c01c9314ace29c21720e8f37ab
이희병 <101461544+22-bottle@users.noreply.github.com>
2024-07-17 22:09:55 +0900

    [Bronze II] Title: 나머지, Time: 80 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            set.add(Integer.valueOf(br.readLine()) % 42);
+        }
+        System.out.println(set.size());
+    }
+}
