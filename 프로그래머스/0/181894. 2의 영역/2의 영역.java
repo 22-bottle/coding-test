1f1b967b112a090bd31ddb371e4d642e66407663
이희병 <101461544+22-bottle@users.noreply.github.com>
2024-07-26 14:06:09 +0900

    [level 0] Title: 2의 영역, Time: 2.79 ms, Memory: 94.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int s = -1, e = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                if (s == -1) s = i;
+                e = i;
+            }
+        }
+        if (s == -1) return new int[] {-1};
+        return Arrays.copyOfRange(arr, s, e + 1);
+    }
+}
