17979551274b290ddf21613c4f2cecb9d0d1c304
이희병 <101461544+22-bottle@users.noreply.github.com>
2024-07-29 22:27:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 620 ms, Memory: 59332 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static PriorityQueue<Point> pq = new PriorityQueue<>((o1, o2) -> {
+        if (o1.x == o2.x) return o1.y - o2.y;
+        return o1.x - o2.x;
+    });
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            pq.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Point point = pq.poll();
+            sb.append(point.x).append(" ").append(point.y).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
