c50bc38d6a568cb56fbd2c669cc6dd2b0ecafc75
이희병 <101461544+22-bottle@users.noreply.github.com>
2025-02-26 21:58:16 +0900

    [level 3] Title: 징검다리 건너기, Time: 58.61 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -2,23 +2,30 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    public int solution(int[] stones, int k) {
-        int left = 0, right = 200_000_000;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            
-            int count = 0, max = 0;
-            for (int stone : stones) {
-                if (stone <= mid) count++;
-                else {
-                    max = Integer.max(max, count);
-                    count = 0;
-                }
-            }
-            max = Integer.max(max, count);
-            if (max < k) left = mid + 1;
-            else right = mid;
+    class Node {
+        int idx;
+        int val;
+        
+        public Node(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
         }
-        return left;
+    }
+    
+    static Deque<Node> dq = new ArrayDeque<>();
+    
+    public int solution(int[] stones, int k) {
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < stones.length; i++) {
+            int stone = stones[i];
+            while (!dq.isEmpty() && i - dq.peekFirst().idx >= k)
+                dq.pollFirst();
+            while (!dq.isEmpty() && dq.peekLast().val < stone)
+                dq.pollLast();
+            dq.addLast(new Node(i, stone));
+            if (i >= k - 1)
+                answer = Integer.min(answer, dq.peekFirst().val);
+        }
+        return answer;
     }
 }
